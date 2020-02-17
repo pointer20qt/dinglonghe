d10d2ad64745ffd971d4dc0489100fe914a5630c
zhuqinran <2088703697@qq.com>
2020-02-17 17:23:05 +0800

    shared_ptr指针 2020/2/17

---
+++ unique_ptr.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <memory>
+/*任务1：unique_ptr练习实现
+任务2：shared_ptr实现，weak_ptr实现*/
+class A
+{
+public:
+	A()
+	{
+		cout << "A构造了" << endl;
+	}
+	~A()
+	{
+		cout << "A析构了" << endl;
+	}
+};
+int main1()
+{
+	unique_ptr<A>ptr1(new A());
+	unique_ptr<A>ptr2(move(ptr1)); //可以移动构造
+	unique_ptr<A>ptr3 = move(ptr2);//可以移动赋值
+	return 0;
+	system("pause");
+}
